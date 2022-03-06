b927fcb492f2a8ac7f9e25b1eb81cda78cbcf00a
pradeep Lamichhane <pradeep@techexchange.in>
2022-03-06 00:21:19 +0000

    Changes

--- Travelling Backend/CLI_packing.py
+++ Travelling Backend/CLI_packing.py
@@ -19,7 +19,7 @@ class CLI_Packing:
 
         #This will print some informations about our website to the user and ask for the informations about the package with the user.
 
-        print(f'WELCOME TO THE ADVENTURE NEPAL!!!,{customer_username}'+ '\n'+ 'What kind of packages do you like to get?'+'\n+NATURAL'+'\n+CULTURAL'+'\n+ADVENTUROUS'+'\n'+'Name the place you would love to visit! ')
+        print(f'WELCOME TO THE ADVENTURE NEPAL!!!'+ '\n'+ 'What kind of packages do you like to get?'+'\n+NATURAL'+'\n+CULTURAL'+'\n+ADVENTUROUS'+'\n'+'Name the place you would love to visit! ')
         user_choice_package_name=input('Enter the name of place you would like to visit in Nepal!!: ')
         user_choice_package_type=input('Enter the package type you want:Natural / Cultural / Adventurous ?')
         user_choice_number_of_people=input('How many people are you travelling? ')
